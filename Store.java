26eb26de79149eb37fed44faa0ccc1ad55b79186
tdg2205 <143349219+tdg2205@users.noreply.github.com>
2023-12-11 16:46:09 -0500

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
+/**
+ * Store.java
+ * <p>
+ * This is a Store.java class which contains all the fields, getters and setters method along with increasing purchase count and updating quanity methods.
+ *
+ * @author Tyler Gentry and Krish Sharma, lab sec 30
+ * @version 11 November, 2023
+ */
 public class Store {
     private ArrayList<Product> storeProducts;
-    private  String storeName;
+    private String storeName;
     private User owner;
     private ArrayList<Customer> purchases;
 
@@ -38,7 +46,7 @@ public class Store {
         return purchases;
     }
 
-    public Store( String storeName, User owner) {
+    public Store(String storeName, User owner) {
         this.storeProducts = new ArrayList<>();
         this.storeName = storeName;
         this.owner = owner;
