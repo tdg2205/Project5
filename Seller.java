59ce01e67176990e823af6c9960133bd5486707f
tdg2205 <143349219+tdg2205@users.noreply.github.com>
2023-12-11 16:45:51 -0500

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
-
-public class Seller extends User{
-    public  ArrayList<Store> yourStores;
+/**
+ * Seller.java
+ * <p>
+ * This is a Seller.java class which contains all the fields, getters and setters method
+ * with methods to edit and remove products along with a method to create stores
+ *
+ * @author Lohit Jagarlamudi and Tyler Gentry, lab sec 30
+ * @version 11 November, 2023
+ */
+public class Seller extends User {
+    public ArrayList<Store> yourStores;
 
     public Seller(String email, String password, String username) {
         super(email, password, username, true);
@@ -11,26 +19,29 @@ public class Seller extends User{
     public void setStores(ArrayList<Store> stores) {
         yourStores = stores;
     }
+
     public ArrayList<Store> getStores() {
-        return  yourStores;
+        return yourStores;
     }
 
-    public  ArrayList<Store> getYourStores() {
+    public ArrayList<Store> getYourStores() {
         return yourStores;
     }
 
 
-    public void createYourStore(Store store){
+    public void createYourStore(Store store) {
         yourStores.add(store);
     }
+
     public static void addProduct(Product product, Store store) {
         store.getProducts().add(product);
     }
+
     public static void removeProduct(Product product, Store store) {
         if (store.getProducts().isEmpty()) {
             System.out.println("No products in store");
         } else {
-            for (Product p: store.getProducts()) {
+            for (Product p : store.getProducts()) {
                 if (p.equals(product)) {
                     store.getProducts().remove(product);
                     return;
@@ -45,7 +56,7 @@ public class Seller extends User{
         if (store.getProducts().isEmpty()) {
             System.out.println("No products in store");
         } else {
-            for (Product p: store.getProducts()) {
+            for (Product p : store.getProducts()) {
                 if (p.equals(product)) {
                     p.setProductName(newName);
                     p.setDescription(newDescription);
